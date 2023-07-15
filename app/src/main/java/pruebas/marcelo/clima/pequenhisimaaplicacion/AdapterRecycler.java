8ce15190c1183d55e16c4bb0d709caef225983a0
Marvid21 <arguellomarcelo48@gmail.com>
2023-07-15 01:54:43 -0400

    creado recyclerView

---
+++ app/src/main/java/pruebas/marcelo/clima/pequenhisimaaplicacion/AdapterRecycler.java
@@ -0,0 +1,63 @@
+package pruebas.marcelo.clima.pequenhisimaaplicacion;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterRecycler extends RecyclerView.Adapter<AdapterRecycler.ViewHolderDatos> implements View.OnClickListener {
+
+    ArrayList<String> listDatos;
+    private View.OnClickListener listener;
+
+    public AdapterRecycler(ArrayList<String> listDatos){
+        this.listDatos = listDatos;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list,null,false);
+        view.setOnClickListener(this);
+        return new ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
+        holder.asignarDatos(listDatos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listDatos.size();
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(listener != null){
+            listener.onClick(v);
+        }
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+        TextView tvItem;
+        public ViewHolderDatos(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(R.id.tvItem);
+        }
+
+        public void asignarDatos(String dato) {
+            tvItem.setText(dato);
+        }
+    }
+}
--- app/src/main/java/pruebas/marcelo/clima/pequenhisimaaplicacion/ListaCiudadesActivity.java
+++ app/src/main/java/pruebas/marcelo/clima/pequenhisimaaplicacion/ListaCiudadesActivity.java
@@ -1,16 +1,53 @@
 package pruebas.marcelo.clima.pequenhisimaaplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import pruebas.marcelo.clima.pequenhisimaaplicacion.R;
 
 public class ListaCiudadesActivity extends AppCompatActivity {
 
+    ArrayList<String> listDatos;
+    RecyclerView rvCities;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_ciudades);
+
+        rvCities = findViewById(R.id.rvCities);
+        rvCities.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        listDatos = new ArrayList<String>();
+
+//        for(int i=0; i<=50; i++){
+//            listDatos.add("Dato #"+i+" ");
+//        }
+        listDatos.add("Asunción");
+        listDatos.add("Ciudad del Este");
+        listDatos.add("Encarnación");
+        listDatos.add("Loma Plata");
+        listDatos.add("Villa Rica");
+
+        AdapterRecycler adapterRecycler = new AdapterRecycler(listDatos);
+
+        adapterRecycler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "Presionaste: "+listDatos.get(rvCities.getChildAdapterPosition(v)))
+                Intent iCiudad = new Intent(getBaseContext(), DetalleClimaCiudadActivity.class);
+                startActivity(iCiudad);
+            }
+        });
+
+        rvCities.setAdapter(adapterRecycler);
     }
 }
