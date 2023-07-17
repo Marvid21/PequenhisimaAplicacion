403060444cece33142174f4ed7615a60b08848c7
Marvid21 <arguellomarcelo48@gmail.com>
2023-07-17 08:17:59 -0400

    ocultamos APIKEY

--- app/src/main/java/pruebas/marcelo/clima/pequenhisimaaplicacion/retrofit/ApiInterface.java
+++ app/src/main/java/pruebas/marcelo/clima/pequenhisimaaplicacion/retrofit/ApiInterface.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
-    @GET("weather?appid=e38a45ab04f410c8b0956c5aeb3d8ab9&units=metric")
+    @GET("weather?appid=&units=metric")
     Call<MainClase> getWeatherData(@Query(value = "q", encoded = true) String city);
 }
